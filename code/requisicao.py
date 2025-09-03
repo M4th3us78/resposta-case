c4b764a4b1455a6c67dbe836c73c8be9c94beac9
matheus <matheus22car@gmail.com>
2025-09-03 00:12:51 -0300

    Criação do arquivo de processamento do data frame

--- code/requisicao.py
+++ code/requisicao.py
@@ -5,8 +5,6 @@ import pandas as pd
 def coletar_noticias(termo_busca):
     url = f"https://news.google.com/rss/search?q={termo_busca}&hl=pt-BR&gl=BR&ceid=BR:pt-419"
 
-
-
     try:
         response = requests.get(url)
         response.raise_for_status()
@@ -26,7 +24,7 @@ def coletar_noticias(termo_busca):
                     'link' : link,
                     'descricao' : descricao
                 })
-        return dados_noticias[:15]
+        return dados_noticias[:50]
     
     except requests.exceptions.RequestException as e:
         print(f"Erro ao coletar notícias: {e}")
@@ -35,16 +33,18 @@ def coletar_noticias(termo_busca):
         print(f"Erro ao analisar XML: {e}")
         return []
 
-termo = "inteligencia+artificial+no+piaui"
-noticias = coletar_noticias(termo)
+def coleta():
+    termo = "inteligencia artificial no piaui"
+    noticias = coletar_noticias(termo)
 
-if noticias:
-    df_noticias = pd.DataFrame(noticias)
-    print(f"Quantidades de noticias coletadas: {len(df_noticias)}")
-    print(df_noticias)
-else:
-    print("Nenhuma notícia encontrada.")
+    if noticias:
+        df_noticias = pd.DataFrame(noticias)
+        print(df_noticias)
+        return df_noticias
+    else:
+        print("Nenhuma notícia encontrada.")
 
+coleta()
 
 
 
